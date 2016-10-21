2b6de08a99354c646df309f32f4cd0bdedad1074
seb <sebbie777@gmail.com>
2016-10-20 21:53:52 -0600

    class for connecting to database

---
+++ src/database/JDBCConnection.java
@@ -0,0 +1,42 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+	
+	
+	private String url = "jdbc:mysql://localhost:3306/stocks";
+	private String userName = "root";
+	private String password = "root";
+	
+	public JDBCConnection(){
+		
+	}
+	
+	public JDBCConnection(String url, String userName, String password){
+		this.url = url;
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	public Connection connectToDataBase() {
+		
+		Connection connection = null;
+		System.out.println("Connecting database...");
+		
+		try {
+
+			connection = DriverManager.getConnection(url, userName, password);
+			System.out.println("Database connected!");
+
+		} catch (SQLException e) {
+
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
+		
+		return connection;
+	}
+	
+}
