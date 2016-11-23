056f6ae5cb0e844869046a220d6519e0239797aa
seb <sebbie777@gmail.com>
2016-11-22 20:35:58 -0700

    Fixed simple moving avg

--- src/database/TechnicalSignals.java
+++ src/database/TechnicalSignals.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
-
 public class TechnicalSignals
 {
 	/**
@@ -15,19 +13,33 @@ public class TechnicalSignals
 	 * @param date
 	 * @param symbol
 	 */
-	public void simpleMovingAverage(Integer days,String exchange, String date, String symbol)
+	public void simpleMovingAverage(Integer days, String exchange, String date, String symbol)
 	{
 		JDBCConnection jdbcConnection = new JDBCConnection();
 		Connection connection = jdbcConnection.connectToDataBase();
-		String query2 = "select round(avg(close),4) from " + exchange +  " where date <='" + date +"' AND symbol ='"+ symbol+"' ORDER BY date DESC LIMIT "+ days ;		
+		String query = "select round(avg(close),4) from " + exchange + " where date <='" + date + "' AND symbol ='"
+				+ symbol + "' ORDER BY date DESC LIMIT " + days;
+		String query2 = "select count(*) from " + exchange + " where date <='" + date + "' AND symbol ='" + symbol
+				+ "' ORDER BY date DESC";
 		try
 		{
 			java.sql.Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(query2);
-			while (resultSet.next())
+			ResultSet resultSet = statement.executeQuery(query);
+			java.sql.Statement statement2 = connection.createStatement();
+			ResultSet resultSet2 = statement2.executeQuery(query2);
+
+			while (resultSet2.next())
 			{
-				System.out.println(resultSet.getString(1));
+				if(Integer.parseInt(resultSet2.getString(1)) < days){
+					System.out.println("Not enough dates to get this Moving Average");
+					break;
+				}
+				while (resultSet.next())
+				{
+					System.out.println(resultSet.getString(1));
+				}
 			}
+			
 		} catch (SQLException e)
 		{
 			e.printStackTrace();
@@ -41,4 +53,31 @@ public class TechnicalSignals
 		}
 	}
 
+	public void getMACrossover(String symbol, String exchange)
+	{
+
+		JDBCConnection jdbcConnection = new JDBCConnection();
+		Connection connection = jdbcConnection.connectToDataBase();
+		String query = "select * from " + exchange + " where symbol = '" + symbol + "' ORDER BY date DESC";
+		try
+		{
+			java.sql.Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next())
+			{
+				simpleMovingAverage(10, "amex", resultSet.getString(2), "aau");
+				System.out.println(resultSet.getString(2));
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			connection.close();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
